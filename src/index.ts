0b21c33c1854c258fe64ef35cb9c08748b9ff601
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-06-07 12:31:35 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,19 @@
 let cantidadTotal: number[] = new Array(270);
-let indice: number;
 let menores: number = 0;
 let mayores: number = 0;
 
-for (indice = 0; indice < 270; indice++) {
+function valorEdades(tope: number): number {
+  return Math.floor(Math.random() * tope);
+}
+
+for (let indice = 0; indice < 270; indice++) {
   cantidadTotal[indice] = valorEdades(41);
   if (cantidadTotal[indice] >= 21) {
     mayores++;
-  } else if (cantidadTotal[indice] < 21) {
+  } else {
     menores++;
   }
 }
 console.log("La cantidad total de personas es: ", cantidadTotal);
 console.log("La cantidad de personas MAYORES a 21 es:", mayores);
 console.log("La cantidad de personas MENORES a 21", menores);
-
-function valorEdades(tope: number): number {
-  return Math.floor(Math.random() * tope);
-}
